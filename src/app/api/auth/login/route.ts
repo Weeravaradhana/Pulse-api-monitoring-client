4ffda67700510376864b6e8cc38fa0f3534db0ba
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-08 12:56:33 +0530

    feat(auth): extend login API to store refresh token in httpOnly cookies

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -7,13 +7,12 @@ export async function POST(request: NextRequest){
     try {
         const body = await request.json();
         const { email, password } = body;
-
         const backendResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
             email,
             password
         });
 
-        const {accessToken, user} = backendResponse.data;
+        const {accessToken, user, refreshToken} = backendResponse.data;
         const response = NextResponse.json({success: true, user});
 
         setCookie('accessToken', accessToken, {
@@ -25,6 +24,15 @@ export async function POST(request: NextRequest){
             maxAge: 15 * 60,
             path: '/'
         });
+        setCookie('refreshToken',refreshToken, {
+            req: request,
+            res: response,
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 7*24*60*60,
+            path: '/'
+        })
         return response;
 
     }catch (error: unknown){
