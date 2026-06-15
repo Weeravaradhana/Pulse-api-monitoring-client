3222b2ffe0108339a21c17ce3f88d0c929d1fed4
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-15 12:52:38 +0530

    refactor(ui): simplify Toggle component by removing internal state and aligning behavior with controlled props

--- src/components/notification/toggle.tsx
+++ src/components/notification/toggle.tsx
@@ -1,16 +1,31 @@
+import {useState} from "react";
+
 type ToggleProps = {
     enabled: boolean;
     onChange: (value: boolean) => void;
+    onSelect?: (value: boolean) => void;
+
 };
 
-export default function Toggle({ enabled, onChange }: ToggleProps) {
+export default function Toggle({ enabled, onChange, onSelect }: ToggleProps) {
+    const [isEnable, setEnable] = useState(true);
+    console.log(isEnable)
+
+    const selectToggle = (selectedData: boolean) => {
+        setEnable(selectedData)
+        onChange(isEnable)
+        if (onSelect) {
+            onSelect(selectedData);
+        }
+    };
+
     return (
         <button
             role="switch"
             aria-checked={enabled}
-            onClick={() => onChange(!enabled)}
+            onClick={() => selectToggle(!isEnable)}
             className={`relative inline-flex h-5 w-9 items-center rounded-full transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
-                enabled ? "bg-indigo-500" : "bg-gray-200"
+                enabled ? "bg-blue-500" : "bg-gray-200"
             }`}
         >
       <span
