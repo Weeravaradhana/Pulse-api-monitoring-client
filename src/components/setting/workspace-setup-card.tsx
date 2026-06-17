a216b2149d50f4831c420a653a6122514151cbe2
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-17 11:08:48 +0530

    `refactor: integrate react-hook-form and zod validation into WorkspaceSetupCard`

--- src/components/setting/workspace-setup-card.tsx
+++ src/components/setting/workspace-setup-card.tsx
@@ -1,27 +1,78 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Card } from "../common/card";
 import { Input } from "../common/input";
 import { Button } from "../common/button";
+import {useForm} from "react-hook-form";
+import {tenantSchema, TenantSchema} from "@/validators/tenant-create.schema";
+import {zodResolver} from "@hookform/resolvers/zod";
+import axios, {AxiosError} from "axios";
 
-export function WorkspaceSetupCard() {
-    const [org, setOrg] = useState("TravelEase");
-    const [slug, setSlug] = useState("");
+export const  WorkspaceSetupCard = ({onTenantCreate}: {onTenantCreate: (id: string) => void}) =>{
+    const [apiError, setApiError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: {errors, isValid}
+
+    } = useForm<TenantSchema>({
+        resolver: zodResolver(tenantSchema),
+        mode: 'onChange',
+        defaultValues: {
+            name: '',
+            slug: '',
+        }
+    })
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const nameValue = e.target.value;
+        setValue('name', nameValue, {shouldValidate: true});
+
+        const generateSlug = nameValue
+            .toLowerCase()
+            .replace(/[^a-z0-9]/g, '-')
+            .replace(/(^-|-$)+/g, '')
+        setValue('slug', generateSlug, {shouldValidate: true})
+    }
+
+   const onSubmit = async (data: TenantSchema) =>{
+        setLoading(true);
+        setApiError(null);
+
+        try {
+            const response = await axios.post('http://localhost:3000/tenant', data, {
+                withCredentials: true
+            });
+
+            if (response){
+                alert('Organization Registered Successfully!');
+                onTenantCreate(response.data.id);
+            }
+        }catch (error) {
+          if (error instanceof AxiosError){
+              setApiError(error.response?.data.message || 'Failed to create organization')
+          }
+        }finally {
+            setLoading(false)
+        }
+    }
+
 
     return (
+        <form onSubmit={handleSubmit(onSubmit)}>
         <Card>
 
             <h3 className="text-sm font-semibold mb-4">Workspace</h3>
 
             <div className="space-y-3">
-                <Input value={org} onChange={(e) => setOrg(e.target.value)} />
+                <Input {...register('name')} placeholder={"Tenant name"} onChange={handleNameChange}/>
 
                 <Input
-                    value={slug}
-                    onChange={(e) =>
-                        setSlug(e.target.value.toLowerCase().replace(/\s+/g, "-"))
-                    }
+                    {...register('slug')}
                     placeholder="workspace-slug"
                 />
 
@@ -29,5 +80,6 @@ export function WorkspaceSetupCard() {
             </div>
 
         </Card>
+        </form>
     );
 }
